6b7007701094856b332223f8647b1ba8c2e85499
Ramon Sousa <chili-guy@users.noreply.github.com>
2026-03-15 10:33:07 -0300

    fix: use root base path for Vercel/Root deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/PORTFOLIO_WEBSITE/' : '/',
+  base: '/',
   plugins: [react()],
   build: {
     rollupOptions: {
